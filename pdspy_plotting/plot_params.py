3ccc5a0b493905ef780312113f7c678a754f1ce9
Blake Drechsler <wbd814@gmail.com>
2025-08-25 13:08:03 -0400

    removed param file

--- pdspy_plotting/plot_params.py
+++
@@ -1,6 +0,0 @@
-source = "L1527IRS" # name of the source
-v_width = 6.0 # velocity range to plot in km/s
-models = ['dartois-exptaper-final', 'dartois-trunc', 'exptaper', 'truncated'] # models to plot
-base_path = "/net/galactica/export/data_1/jtobin/pdspy-modeling/NASA" # root directory of models
-plot_type = 'seperate' # determine to make one plot or split into seperate blue and red shifted plots
-ncpu=40 # number of cpus
