69afe9cd4fae10d02d567f9c90da5be3ee4605ab
Blake Drechsler <wbd814@gmail.com>
2025-09-21 09:11:34 -0700

    created default parameter file

---
+++ pdspy_plotting/plot_params_defaults.py
@@ -0,0 +1,6 @@
+source = "" # name of the source
+v_width = 6.0 # velocity range to plot in km/s
+models = []  # models to plot
+base_path = ".." # root directory of models
+plot_type = 'full' # determine to make one plot or split into seperate blue and red shifted plots
+ncpu=8 # number of cpus
