5fc972e8d52d8d0d3fcc2408c311196781718983
Blake Drechsler <wbd814@gmail.com>
2025-09-26 11:54:05 -0400

    fixed typo

--- pdspy_plotting/main.py
+++ pdspy_plotting/main.py
@@ -150,7 +150,7 @@ def main():
             print('making red plot')
             plot_grid(model, visibilities, m_adj, params, params_adj, residual,
                       v_start_r, v_end_r, 0, 'BlueToRed', 'k', 7, 1,
-                      v_width, 'red', 3, plot_params.ncols, plot_params.levels plot_params.negative_levels)
+                      v_width, 'red', 3, plot_params.ncols, plot_params.levels, plot_params.negative_levels)
         if plot_params.plot_type == 'full':
             print('making full plot')
             plot_grid(model, visibilities, m_adj, params, params_adj, residual,
